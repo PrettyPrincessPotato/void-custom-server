e898cd39d6ccc264e4af455a809ce4ed3f8c7a5f
lunam <email@gmail.com>
2026-08-21 21:12:42 -0500

    Reformatted GoblinRaids and cleaned up the code a lot

--- game/src/main/kotlin/content/activity/city_raids/GoblinRaids.kt
+++ game/src/main/kotlin/content/activity/city_raids/GoblinRaids.kt
@@ -2,10 +2,9 @@ package content.activity.city_raids
 
 import content.entity.combat.dead
 import content.entity.combat.inCombat
-import content.entity.combat.target
+import content.skill.slayer.categories
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.instruction.handle.interactNpc
-import world.gregs.voidps.engine.client.instruction.handle.interactObject
 import world.gregs.voidps.engine.entity.World
 import world.gregs.voidps.engine.entity.character.mode.EmptyMode
 import world.gregs.voidps.engine.entity.character.mode.Patrol
@@ -16,16 +15,47 @@ import world.gregs.voidps.engine.timer.toTicks
 import world.gregs.voidps.type.Tile
 import java.util.concurrent.TimeUnit
 
-const val debugMe = false
+const val DEBUG = false
 
+//IDEA: What if other creatures don't like each-other? Like a dragon and a goblin get assigned to the same camp, there's now a turf war to see who wins. (Just make them fight and whoever lives wins)
+
+/**
+ * Identifying states for the LSM
+ */
+private data class GoblinProgress(
+    val state: RaidState,
+    val destination: RaidDestination
+)
+private enum class RaidState{
+    TRAVELLING_TO_CAMP,
+    MUSTERING,
+    TRAVELLING_TO_TOWN,
+    SIEGING_TOWN
+}
+private enum class RaidDestination {
+    FALADOR,
+    VARROCK
+}
+
+/**
+ * Locations
+ */
 private val GOBLIN_RAID_VARROCK_AREA : Tile = Tile(3175, 3428) //TODO: Change to a different location and get path
 private val GOBLIN_CAMP_FALADOR : Tile = Tile(2953, 3407) //TODO: Remove trees when they muster
 private val GOBLIN_ATTACK_FALADOR : Tile = Tile(2966, 3394)
-
 private val GOBLIN_VILLAGE_TILE : Tile = Tile(2956, 3503) // Arbitrary tile in goblin village to spawn our war goblins.
 
+/**
+ * Mob IDs
+ */
 private val GOBLIN_IDS = intArrayOf(3264, 3265, 3266, 3267) //TODO: Diversify spawns. Low prio.
+// Just an example low level goblin that happens to share the string name with the ID. String name is needed.
+private const val GOBLIN_ID_TEST = "3264"
 
+/**
+ * Routes - list of tiles from a to b.
+ * TODO: Find out if there's an easier way to get paths so we only need the destination tile.
+ */
 // A list of tiles determining a (somewhat) clean path from Goblin Village to just outside Falador.
 private val GOBLIN_VILLAGE_TO_FALADOR_CAMP = listOf(
     Tile(2956, 3499),
@@ -47,144 +77,161 @@ private val FALADOR_CAMP_TO_FALADOR_GATE = listOf(
     Tile(2966, 3398),
     Tile(2966, 3394)
 )
-// Just an example low level goblin that happens to share the string name with the ID. String name is needed.
-private const val GOBLIN_ID_TEST = "3264"
 
 class GoblinRaids : Script {
-    val waitingForPatrolEnd = mutableSetOf<NPC>()
-    val musteringInFalador = mutableSetOf<NPC>()
-    val attackingFalador = mutableSetOf<NPC>()
-    val activeGobbos = mutableSetOf<NPC>()
+    private val goblinProgress = mutableMapOf<NPC, GoblinProgress>()
 
     init {
-        if(debugMe){println("GoblinRaids.kt Loaded...")}
+        if(DEBUG){println("GoblinRaids.kt Loaded...")}
+
+        /**
+         * Declaring who can target what.
+         */
         huntNPC("aggressive_npcs") { target ->
-            if (id.startsWith("guard_fal") && target.id.startsWith("") && !target.id.startsWith("guard_fal")) {
+            if (id.startsWith("guard_fal") && target.categories.contains("goblins") && !target.id.startsWith("guard_fal")) {
                 interactNpc(target, "Attack")
             }
-            if (id == GOBLIN_ID_TEST && target.id.startsWith("") && target.id != GOBLIN_ID_TEST) {
+            if (id == GOBLIN_ID_TEST && target.id != GOBLIN_ID_TEST) {
                 interactNpc(target, "Attack")
             }
         }
 
         worldSpawn {
-            if(debugMe){println("GoblinRaids - worldSpawn")}
+            if(DEBUG){println("GoblinRaids - worldSpawn")}
             World.timers.start("goblin_raid_timer")
-            World.timers.start("goblin_mode_check_timer")
-            World.timers.start("goblin_count_check_timer")
+            World.timers.start("polling_timer")
         }
 
+        /**
+         * Goblin Spawning timer
+         */
         worldTimerStart("goblin_raid_timer") { TimeUnit.MINUTES.toTicks(5) }
         worldTimerTick("goblin_raid_timer") {
-            if(debugMe){println("spawngobbo")}
-            spawnGobbo(pathTo = GOBLIN_VILLAGE_TO_FALADOR_CAMP)
+            // Spawn our lil gobbo and send them out, repeat every time unit above ^
+            if(DEBUG){println("spawngobbo")}
+            val gobbo = NPCs.addRandom(GOBLIN_ID_TEST, GOBLIN_VILLAGE_TILE.toCuboid(5)) ?: NPCs.add(GOBLIN_ID_TEST, GOBLIN_VILLAGE_TILE)
+            setRaidState(gobbo, RaidState.TRAVELLING_TO_CAMP, RaidDestination.FALADOR)
             Timer.CONTINUE
         }
 
-        worldTimerStart("goblin_mode_check_timer") { TimeUnit.MILLISECONDS.toTicks(300) } // check ~3x/sec
-        worldTimerTick("goblin_mode_check_timer") {
-            // Copy to avoid concurrent modification while removing
-            val iteratorSnapshot = waitingForPatrolEnd.toList()
+        /**
+         * Polling timer. ~3x a second.
+         */
+        worldTimerStart("polling_timer") { TimeUnit.MILLISECONDS.toTicks(300) }
+        worldTimerTick("polling_timer") {
+            val trackedGoblins = goblinProgress.toList()
+            val musteringGoblins = goblinsInState(RaidState.MUSTERING)
+            val travelingToCampGoblins = goblinsInState(RaidState.TRAVELLING_TO_CAMP)
+            val travellingToTownGoblins = goblinsInState(RaidState.TRAVELLING_TO_TOWN)
 
-            for (gobboAny in iteratorSnapshot) {
-                if (gobboAny.mode == EmptyMode && !gobboAny.inCombat ) { //TODO: Figure out a more graceful "is busy" solution
-                    // We are at the encampment, now we wait to attack
-                    // AI seems to be wonky, so let's make a new goblin.
-                    gobboAny.despawn(0)
-                    val gobboNew = NPCs.add(GOBLIN_ID_TEST, GOBLIN_CAMP_FALADOR)
-                    gobboNew.huntMode = "aggressive_npcs"
-                    //can do gobboNew.interactObject(TreeObject) once we find nearby trees to have the goblins chop one down nearby. No shot they have anims...
-
-
-                    if(debugMe){
-                        println("Applied Hunt Mode: ${gobboNew}, ${gobboNew.huntMode}")
-                    }
-                    waitingForPatrolEnd.remove(gobboAny) // This might cause issues since I despawn the NPC already. Let's find out!
-                    musteringInFalador.add(gobboNew)
-                    activeGobbos.remove(gobboAny)
-                    activeGobbos.add(gobboNew)
+            // Despawning all dead mobs so the world isn't flooded with mobs
+            for ((gobbo, progress) in trackedGoblins) {
+                if (gobbo.dead) {
+                    goblinProgress.remove(gobbo)
+                    gobbo.despawn(8)
+                    continue
                 }
             }
+
+            //Count how many goblins are mustering. Will need to consider location here, too.
+            //TODO: Expand to accept multiple possible camps.
+            if(musteringGoblins.size >= 5){
+                for((gobbo, progress) in goblinProgress){
+                    if(progress.state == RaidState.MUSTERING && progress.destination == RaidDestination.FALADOR){
+                        setRaidState(gobbo, RaidState.TRAVELLING_TO_TOWN, RaidDestination.FALADOR)
+                    }
+                }
+            }
+
+            /**
+             * Check up on our traveling goblins.
+             */
+            for(gobbo in travelingToCampGoblins){
+                if (gobbo.mode == EmptyMode && !gobbo.inCombat){
+                    // We are at the encampment, now we wait to attack
+                    // For some reason I can't set a new tile as spawn, and I'm unable to re-set huntMode, so let's make a new goblin to clean up whatever's going on there.
+                    gobbo.despawn(0)
+                    val gobboNew = NPCs.add(GOBLIN_ID_TEST, GOBLIN_CAMP_FALADOR)
+                    gobboNew.huntMode = "aggressive_npcs"
+                    setRaidState(gobboNew, RaidState.MUSTERING, RaidDestination.FALADOR)
+                }
+            }
+            for(gobbo in travellingToTownGoblins){
+                if(gobbo.mode == EmptyMode && !gobbo.inCombat ) { //TODO: Figure out a more graceful "is busy" solution
+                    gobbo.despawn(0)
+                    val gobboNew = NPCs.add(GOBLIN_ID_TEST, GOBLIN_ATTACK_FALADOR)
+                    gobboNew.huntMode = "aggressive_npcs"
+                    setRaidState(gobboNew, RaidState.SIEGING_TOWN, RaidDestination.FALADOR)
+                }
+            }
+
             Timer.CONTINUE
         }
-        worldTimerStart("goblin_count_check_timer") { TimeUnit.MILLISECONDS.toTicks(300) } // check ~3x/sec
-        worldTimerTick("goblin_count_check_timer") {
-            val iteratorSnapshotAttacking = attackingFalador.toList()
-            val iteratorSnapshotInCombat = activeGobbos.toList()
+    }
 
-            if(musteringInFalador.count() >= 5){
-                val iteratorSnapshotMustering = musteringInFalador.toList()
+    private fun setRaidState(
+        gobbo: NPC,
+        state: RaidState,
+        destination: RaidDestination
+    ) {
+        goblinProgress[gobbo] = GoblinProgress(state, destination)
+        /**
+         * WHEN STATEMENTS, make sure these don't loop into each-other.
+         */
+        when (state) {
+            RaidState.TRAVELLING_TO_CAMP -> {
+                if(DEBUG){println("Spawned goblin, going to camp")}
 
-                // Tying waypoints from Falador camp to gate where the guards are
+                val waypoints: List<Pair<Tile, Int>> = GOBLIN_VILLAGE_TO_FALADOR_CAMP.map { tile -> //TODO: Make this a variable somehow to expand different camps
+                    tile to 0 // or some delay ticks between tiles
+                }
+                // Walk over to encampment
+                gobbo.mode = Patrol(
+                    character = gobbo,
+                    waypoints = waypoints,
+                    loop = false,
+                    noCollision = false
+                )
+            }
+
+            RaidState.MUSTERING -> {
+                //Find the tree object nearby somehow
+                //can do gobboNew.interactObject(TreeObject) once we find nearby trees to have the goblins chop one down nearby. No shot they have anims...
+            }
+
+            RaidState.TRAVELLING_TO_TOWN -> {
                 val waypoints: List<Pair<Tile, Int>> = FALADOR_CAMP_TO_FALADOR_GATE.map { tile ->
                     tile to 0 // or some delay ticks between tiles
                 }
-                // TODO: Shortcut pathing somehow
-                for(gobboAny in iteratorSnapshotMustering){
-                    gobboAny.mode = Patrol(
-                        character = gobboAny,
-                        waypoints = waypoints,
-                        loop = false,
-                        noCollision = true // Enabled here because they get caught and clump easily. This is more like a goblin rush.
-                    )
-                    attackingFalador.add(gobboAny)
-                    musteringInFalador.remove(gobboAny)
-                }
+                gobbo.mode = Patrol(
+                    character = gobbo,
+                    waypoints = waypoints,
+                    loop = false,
+                    noCollision = true // Enabled here because they get caught and clump easily. This is more like a goblin rush.
+                )
             }
-            // These are attacking and need to be set to wander again once the patrol is done
-            for(gobboAny in iteratorSnapshotAttacking){
-                if(debugMe){println(gobboAny.mode)}
 
-                if(gobboAny.mode == EmptyMode && !gobboAny.inCombat ){ //TODO: Figure out a more graceful "is busy" solution
-                    gobboAny.despawn(0)
-                    val gobboNew = NPCs.add(GOBLIN_ID_TEST, GOBLIN_ATTACK_FALADOR)
-                    gobboNew.huntMode = "aggressive_npcs" // AI for multi-combat attacking inside the city, so focused on NPCs //camp_aggressive
+            RaidState.SIEGING_TOWN -> {
+                /**
+                 * Nothing really special needed here, they're in town. Maybe add something fancy to do here. It'd be *really* cool
+                 * if the various mobs can take an entire town through some minigame, king of the hill or something, but I can see
+                 * some issues with that from a gameplay stance.
+                 * That also may be difficult with this engine.
+                 */
 
-                    attackingFalador.remove(gobboAny)
-                    activeGobbos.remove(gobboAny)
-                    activeGobbos.add(gobboNew)
-                }
             }
-            // These are all the goblins currently existing through this script, we need to make sure that if they die, they stay dead.
-            for(gobboAny in iteratorSnapshotInCombat){
-                if(gobboAny.dead){
-                    gobboAny.despawn(5)
-                    activeGobbos.remove(gobboAny)
-                    //Now we need to clean up the other arrays they happen to be in.
-                    if(gobboAny in waitingForPatrolEnd){
-                        waitingForPatrolEnd.remove(gobboAny)
-                    }
-                    if(gobboAny in musteringInFalador){
-                        musteringInFalador.remove(gobboAny)
-                    }
-                    if(gobboAny in attackingFalador){
-                        attackingFalador.remove(gobboAny)
-                    }
-                }
-            }
-            Timer.CONTINUE
         }
-    }
-}
-
-fun GoblinRaids.spawnGobbo(pathTo: List<Tile>) {
-
-    if(debugMe){println("Starting goblin raid")}
-    val gobbo = NPCs.addRandom(GOBLIN_ID_TEST, GOBLIN_VILLAGE_TILE.toCuboid(5)) ?: NPCs.add(GOBLIN_ID_TEST, GOBLIN_VILLAGE_TILE)
-
-    // Build patrol waypoints
-    val waypoints: List<Pair<Tile, Int>> = pathTo.map { tile ->
-        tile to 0 // or some delay ticks between tiles
+        /**
+         * End of WHEN STATEMENTS
+         */
     }
 
-    // Walk over to encampment (pathTo)
-    gobbo.mode = Patrol(
-        character = gobbo,
-        waypoints = waypoints,
-        loop = false,
-        noCollision = false
-    )
-    // register for global polling
-    activeGobbos.add(gobbo)
-    waitingForPatrolEnd.add(gobbo)
+    private fun goblinsInState(state: RaidState): List<NPC> {
+        return goblinProgress
+            .filterValues { progress ->
+                progress.state == state
+            }
+            .keys
+            .toList()
+    }
 }
