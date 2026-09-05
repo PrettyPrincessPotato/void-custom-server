50ecbdb045c8f55918b0114b3f5645679486c460
Princess <email@gmail.com>
2026-09-05 07:02:29 -0500

    Added notes

--- game/src/main/kotlin/content/activity/city_raids/goblins/GoblinRaidFactory.kt
+++ game/src/main/kotlin/content/activity/city_raids/goblins/GoblinRaidFactory.kt
@@ -5,6 +5,14 @@ import content.activity.city_raids.RaidDestination
 import content.activity.city_raids.RaidFaction
 import content.activity.city_raids.RaidStore
 
+/**
+ * Can't write this down so I"m jotting it here:
+ * goblins DO NOT GIVE A FUCK if you attack them while they're routing.
+ * We also have the ability to use bot navmesh now, so might as well switch it over entirely.
+ * See ShopAssistantFalador.kt for an example
+ * Could make the goblins chant something like "for the big war god" while they're attacking
+ */
+
 class GoblinRaidFactory {
     fun findOrCreateFaladorRaid(store: RaidStore): Raid =
         store.allRaids().firstOrNull {
