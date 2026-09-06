bb3a524c716008fb7115145c3c9a9c5a9e016267
Princess <email@gmail.com>
2026-09-06 07:58:19 -0500

    Commented.

--- game/src/main/kotlin/content/activity/city_raids/goblins/GoblinRaidFactory.kt
+++ game/src/main/kotlin/content/activity/city_raids/goblins/GoblinRaidFactory.kt
@@ -6,11 +6,23 @@ import content.activity.city_raids.RaidFaction
 import content.activity.city_raids.RaidStore
 
 /**
- * Can't write this down so I"m jotting it here:
+ * Can't write this down as a proper issue, so I'm jotting it here:
  * goblins DO NOT GIVE A FUCK if you attack them while they're routing.
+ * Sometimes they'll attack back, but it's rare
  * We also have the ability to use bot navmesh now, so might as well switch it over entirely.
  * See ShopAssistantFalador.kt for an example
  * Could make the goblins chant something like "for the big war god" while they're attacking
+ *
+ * In the issues page I wrote that falador guards and goblins aggro is broken, that is no longer true:
+ * I fixed guards with Guards.kt to resolve aggro there.
+ *
+ * Only semi-related but the reason guards broke was because GuardFalador seems to have gotten overwritten
+ * in a git merge from main. I did spot a new text blurb that made me think... We should have a guard run
+ * back and forth between Falador and whever falador_guard_7 is to provide supplies and also give the player
+ * a hint that someone is over there.
+ *
+ * Also, side-note, look at black and white armor sets/weapons and how they're acquired. That might need
+ * tweaking.
  */
 
 class GoblinRaidFactory {
