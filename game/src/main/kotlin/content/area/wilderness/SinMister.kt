cdfa6f2b117a311012fbf9f5d83bac635886c986
potato <princess@localhost>
2026-09-11 14:52:36 -0500

    Changed the ghost amulet check to use the one from Ghosts Ahoy!

--- game/src/main/kotlin/content/area/wilderness/SinMister.kt
+++ game/src/main/kotlin/content/area/wilderness/SinMister.kt
@@ -14,6 +14,7 @@ import content.entity.player.dialogue.Shifty
 import content.entity.player.dialogue.type.choice
 import content.entity.player.dialogue.type.npc
 import content.entity.player.dialogue.type.player
+import content.quest.member.ghosts_ahoy.checkGhostspeak
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.character.npc.NPC
@@ -43,46 +44,34 @@ class SinMister(graph: NavigationGraph) : Script {
             // NpcSchedules.registry.register(schedule) TEMPORARILY DISABLED
         }
         npcOperate("Talk-to", SIN_MISTER_STRING_ID) {
-            if (equipment.contains("ghostspeak_amulet")) {
-                npc<Neutral>("Hello.")
-                choice {
-                    option<Confused>("What's a ghost doing out here?") {
-                        npc<Neutral>("Why, I'm a banker of course!")
-                        choice {
-                            option<Confused>("What's a ghost banker doing out here?") {
-                                npc<Shifty>("Let's just say I'm... Paying off a debt.")
-                                choice {
-                                    option<Happy>("Well that doesn't at all sound ominous. Can I see my bank please?") {
-                                        openBank()
-                                    }
+            if (!checkGhostspeak()) {
+                return@npcOperate
+            }
+            npc<Neutral>("Hello.")
+            choice {
+                option<Confused>("What's a ghost doing out here?") {
+                    npc<Neutral>("Why, I'm a banker of course!")
+                    choice {
+                        option<Confused>("What's a ghost banker doing out here?") {
+                            npc<Shifty>("Let's just say I'm... Paying off a debt.")
+                            choice {
+                                option<Happy>("Well that doesn't at all sound ominous. Can I see my bank please?") {
+                                    openBank()
                                 }
                             }
                         }
                     }
-                    option("I would like to open my bank account, please.") {
-                        openBank()
-                    }
                 }
-            } else if (inventory.contains("ghostspeak_amulet")) {
-                npc<Idle>("Wooo wooo wooooo!")
-                player<Idle>("Why can't I understand you? Oh, yeah, it might help if I wear this amulet!")
-            } else {
-                sinMister?.say("Woo.. Wooo!!")
-                sinMister?.face(this)
-                message("Sadly, you don't speak ghost.")
+                option("I would like to open my bank account, please.") {
+                    openBank()
+                }
             }
         }
         npcOperate("Bank", SIN_MISTER_STRING_ID) {
-            if (equipment.contains("ghostspeak_amulet")) {
-                openBank()
-            } else if (inventory.contains("ghostspeak_amulet")) {
-                npc<Idle>("Wooo wooo wooooo!")
-                player<Idle>("Why can't I understand you? Oh, yeah, it might help if I wear this amulet!")
-            } else {
-                sinMister?.say("Woo.. Wooo!!")
-                sinMister?.face(this)
-                message("Sadly, you don't speak ghost.")
+            if (!checkGhostspeak()) {
+                return@npcOperate
             }
+            openBank()
         }
     }
 }
