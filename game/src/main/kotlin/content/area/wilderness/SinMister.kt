9066fbbc422f548d72b7251c8d3399e9d732cfde
potato <princess@localhost>
2026-09-13 00:02:24 -0500

    Sinmister is now polite and hands you your amulet if it's in your bank.

--- game/src/main/kotlin/content/area/wilderness/SinMister.kt
+++ game/src/main/kotlin/content/area/wilderness/SinMister.kt
@@ -5,6 +5,7 @@ import content.entity.npc.movement.GraphNpcRouteExecutor
 import content.entity.npc.movement.NativeNpcRouteExecutor
 import content.entity.npc.movement.NpcNavMeshRouteFinder
 import content.entity.npc.movement.NpcRouteExecutor
+import content.entity.player.bank.bank
 import content.entity.player.bank.pin.openBank
 import content.entity.player.dialogue.Confused
 import content.entity.player.dialogue.Happy
@@ -12,11 +13,19 @@ import content.entity.player.dialogue.Neutral
 import content.entity.player.dialogue.Shifty
 import content.entity.player.dialogue.type.choice
 import content.entity.player.dialogue.type.npc
+import content.entity.player.dialogue.type.statement
 import content.quest.member.ghosts_ahoy.checkGhostspeak
 import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.inv.add
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.engine.inv.remove
 
 private const val SIN_MISTER_STRING_ID = "sin_mister"
+private const val GHOSTSPEAK_AMULET_STRING_ID = "ghostspeak_amulet"
+private const val ENCH_GHOSTSPEAK_AMULET_STRING_ID = "ghostspeak_amulet_enchanted"
 
 class SinMister(graph: NavigationGraph) : Script {
     val useBotNav = false
@@ -39,9 +48,10 @@ class SinMister(graph: NavigationGraph) : Script {
             // NpcSchedules.registry.register(schedule) TEMPORARILY DISABLED
         }
         npcOperate("Talk-to", SIN_MISTER_STRING_ID) {
-            if (!checkGhostspeak()) {
+            if(!sinMisterCheckGhostspeak()){
                 return@npcOperate
             }
+
             npc<Neutral>("Hello.")
             choice {
                 option<Confused>("What's a ghost doing out here?") {
@@ -63,10 +73,29 @@ class SinMister(graph: NavigationGraph) : Script {
             }
         }
         npcOperate("Bank", SIN_MISTER_STRING_ID) {
-            if (!checkGhostspeak()) {
+            if(!sinMisterCheckGhostspeak()){
                 return@npcOperate
             }
             openBank()
         }
     }
+}
+
+private suspend fun Player.sinMisterCheckGhostspeak(): Boolean {
+    if(bank.contains(GHOSTSPEAK_AMULET_STRING_ID)) {
+        bank.remove(GHOSTSPEAK_AMULET_STRING_ID)
+        inventory.add(GHOSTSPEAK_AMULET_STRING_ID)
+        statement("Sin Mister hands you your Amulet of Ghostspeak from your bank.")
+        return false
+    }
+    if(bank.contains(ENCH_GHOSTSPEAK_AMULET_STRING_ID)) {
+        bank.remove(ENCH_GHOSTSPEAK_AMULET_STRING_ID)
+        inventory.add(ENCH_GHOSTSPEAK_AMULET_STRING_ID)
+        statement("Sin Mister hands you your Enchanted Amulet of Ghostspeak from your bank.")
+        return false
+    }
+    if (!checkGhostspeak()) {
+        return false
+    }
+    return true
 }
