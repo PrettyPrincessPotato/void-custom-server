dacddb1de5040ff6303614f22f303c6d8f825173
potato <princess@localhost>
2026-09-17 18:19:54 -0500

    Initial Lachtopher schedule edit

--- game/src/main/kotlin/content/area/misthalin/lumbridge/Lachtopher.kt
+++ game/src/main/kotlin/content/area/misthalin/lumbridge/Lachtopher.kt
@@ -1,16 +1,79 @@
 package content.area.misthalin.lumbridge
 
+import content.area.asgarnia.falador.SHOP_AREA
+import content.area.asgarnia.falador.SHOP_TILE
+import content.bot.behaviour.navigation.NavigationGraph
+import content.entity.npc.movement.GraphNpcRouteExecutor
+import content.entity.npc.movement.NpcLocation
+import content.entity.npc.movement.NpcNavMeshRouteFinder
+import content.entity.npc.movement.NpcRouteExecutor
+import content.entity.npc.movement.travelTo
+import content.entity.npc.schedule.NpcScheduleController
+import content.entity.npc.schedule.NpcSchedules
+import content.entity.npc.schedule.ScheduleAction
+import content.entity.npc.schedule.ScheduleTransition
 import content.entity.player.dialogue.Angry
 import content.entity.player.dialogue.Bored
 import content.entity.player.dialogue.Happy
 import content.entity.player.dialogue.Quiz
 import content.entity.player.dialogue.type.npc
 import content.entity.player.dialogue.type.player
+import org.rsmod.game.pathfinder.collision.CollisionStrategies
 import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.type.Tile
 
-class Lachtopher : Script {
+private val LACHTOPHER_HOME = Tile(3233, 3207, 0)
+private val LACHTOPHER_DOOR_OUTSIDE = Tile(3234, 3207, 0)
+private val LACHTOPHER_HANGOUT = Tile(3227, 3237, 0)
+private const val GO_HOME_HOUR = 20
+private const val GO_HANGOUT_HOUR = 6
+private const val LACHTOPHER_STRING_ID = "lachtopher"
+private var LACHTOPHER: NPC? = null
+
+
+class Lachtopher(graph: NavigationGraph) : Script {
+    private val routeExecutor: NpcRouteExecutor = GraphNpcRouteExecutor(NpcNavMeshRouteFinder(graph))
+
+    private val schedule = NpcScheduleController(
+        npcProvider = { LACHTOPHER },
+        routeExecutor = routeExecutor,
+        scheduleTransitions = listOf(
+            ScheduleTransition(
+                GO_HOME_HOUR,
+                ScheduleAction.Custom {
+                    LACHTOPHER?.travelTo(LACHTOPHER_DOOR_OUTSIDE, null, "lachtopher_to_door") {
+
+                        objectOperate("Open", "Door") {
+                            walkToDelay(LACHTOPHER_HOME)
+                            objectOperate("Close", "Door") {}
+                        }
+                    }
+                },
+            ),
+            ScheduleTransition(
+                GO_HANGOUT_HOUR,
+                ScheduleAction.Custom {
+                    LACHTOPHER?.say("This is when I go back!")
+                },
+            ),
+        ),
+    )
 
     init {
+        npcSpawn(LACHTOPHER_STRING_ID) {
+            LACHTOPHER = this
+            this["full_pathfinding"] = true
+            NpcSchedules.registry.register(schedule)
+        }
+        npcDespawn(LACHTOPHER_STRING_ID) {
+            NpcSchedules.registry.unregister(schedule)
+
+            if(LACHTOPHER == this){
+                LACHTOPHER = null
+            }
+        }
+
         npcOperate("Talk-to", "lachtopher") {
             player<Happy>("Hello there.")
             npc<Bored>("Hello, I suppose. I'm Lachtopher. Could you lend me some money?")
