e5888a24ac151afda458b94cf095d93eadef2c98
potato <princess@localhost>
2026-09-23 00:14:28 -0500

    Added sermon lines

--- game/src/main/kotlin/content/schedules/area/misthalin/lumbridge/church/FatherAereckSchedule.kt
+++ game/src/main/kotlin/content/schedules/area/misthalin/lumbridge/church/FatherAereckSchedule.kt
@@ -2,6 +2,23 @@ package content.schedules.area.misthalin.lumbridge.church
 
 import world.gregs.voidps.engine.Script
 
+private val SERMON_SAYINGS = arrayOf(
+    "O, Saradomin! Bless us with your light!",
+    "In Saradomin there is goodness; and that is good.",
+    "Hey, are you asleep back there? Careful, Saradomin is watching.",
+    "... and so Saradomin covered the land in his holy light, blessing the River Salve to keep the undead at bay...",
+    "Our god is the most popular one, and that makes ours the best.",
+    "Together with Zamorak and Guthix, Saradomin helped create the very land we walk on today.",
+    "The legendary warriors known as the Bandos Brothers served under Saradomin, assisting him in conquering southern Forinthry during the God Wars.",
+    "... And the Saradominist wizards were then betrayed by the Zamorakian Cultists, destroying the old wizard's tower in the process...",
+    "From the wise owl, to the graceful unicorn. Saradomin's touch can be found everywhere.",
+    "Go in peace in the name of Saradomin; may his glory shine upon you like the sun.",
+    "Protect your self, protect your friends. Mine is the glory that never ends. This is Saradomin's wisdom.",
+    "The darkness in life may be avoided, by the light of wisdom shining. This is Saradomin's wisdom.",
+    "Show love to your friends, and mercy to your enemies, and know that the wisdom of Saradomin will follow. This is Saradomin's wisdom.",
+    "A fight begun, when the cause is just, will prevail over all others. This is Saradomin's wisdom.",
+    "The currency of goodness is honour; It retains its value through scarcity. This is Saradomin's wisdom.",
+)
 class FatherAereckSchedule : Script {
 
 }
