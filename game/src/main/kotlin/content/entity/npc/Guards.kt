6f1b640ccdf531e00b075d12f6f94601b76a47f0
potato <princess@localhost>
2026-09-25 10:27:16 -0500

    Removed comments

--- game/src/main/kotlin/content/entity/npc/Guards.kt
+++ game/src/main/kotlin/content/entity/npc/Guards.kt
@@ -20,13 +20,10 @@ class Guards : Script {
         npcDeath("*") {
             val guard = killer as? NPC ?: return@npcDeath
             when {
-                // Copy/Pasted from Lumbridge Guards, paramaters changed. Still unsure what val guard does fully other than somehow grabs the needed NPC.
                 guard.id.contains("guard_") -> {
-                    // Name theoretically looks like it can be anything I want.
                     guard.queue("rat_killer") {
                         guard.delay(2)
                         guard.anim("eat_drink")
-                        // From wiki: Every time they kill a monster, they eat a piece of food which heals them to full health, and walk over to the square of their death as to collect a drop.
                         guard.levels.set(Skill.Constitution, guard.levels.getMax(Skill.Constitution))
                         guard.walkTo(tile)
                     }
